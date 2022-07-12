caeed8caaf5add602f3c80b3b6732b0183948bdd
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-07-12 20:52:48 -0300

    atualizando footer

--- museu/src/components/Footer/footer.js
+++ museu/src/components/Footer/footer.js
@@ -9,13 +9,17 @@ export default function footer() {
           <ul>
             <li>
               <img src="/assets/img/email-logo.png" className="email"></img>
-              contato@museutronco.cchla.ufrn.br
+              cirsufrn@gmail.com
             </li>
             <li>
               <img src="/assets/img/phone-logo.png" className="phone"></img>
               (84) 3342-2240
             </li>
           </ul>
+        </div>
+        <div className="logo-ufrn">
+          <img className="ufrn" src="/assets/img/ufrn.png"></img>
+          <img className="uf" src="/assets/img/uf.png"></img>
         </div>
         <div className="logo">
           <img src="/assets/img/green_logo.png" alt="Logo verde do museu Tronco, Ramos e Raízes." />
