3bc15275a5e3cbf283b2c3d1cff7ce973892cae6
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-07-26 14:46:13 -0300

    Implementando página de aúdio #6
    Co-Authored-By: david santos <56547716+daviddevolin@users.noreply.github.com>

--- museu/src/routes.js
+++ museu/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Home from "./pages/home/home.js";
 import Gallery from "./pages/gallery/gallery.js";
 import Movie from "./pages/movies/movies.js";
+import Audio from "./pages/audios/audios.js";
 
 export default function Routes () {
 
@@ -12,6 +13,7 @@ export default function Routes () {
                 <Route path="/" element={<Home />} />
                 <Route path="/gallery" element={<Gallery />} />
                 <Route path="/movie" element={<Movie />} />
+                <Route path="/audio" element={<Audio />} />
             </Switch>
         </BrowserRouter>
     )
