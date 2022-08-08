bc02564ff3ab7ccc11d190df1a4e2d0d378dee8a
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-08-08 14:22:45 -0300

    add video
    Co-Authored-By: Allan Gabriel <43339727+allangbr@users.noreply.github.com>

--- museu/src/pages/movies/movies.js
+++ museu/src/pages/movies/movies.js
@@ -32,6 +32,10 @@ export default function Movie() {
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/dj4jDGNnXr8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/hUEm3RnMkpI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/GbnBZrHGBoY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+            <li><iframe width="560" height="315" src="https://www.youtube.com/embed/DtH-qT9Yh9Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+
+
+
           </ul>
         </div>
       </div>
