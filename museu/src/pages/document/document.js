963faa3acdc5e0826a6577d77ddf6f618f6d431c
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-08-08 14:40:12 -0300

    Adicionando arquivos de documentos #10
    Co-Authored-By: david santos <56547716+daviddevolin@users.noreply.github.com>

--- museu/src/pages/document/document.js
+++ museu/src/pages/document/document.js
@@ -10,14 +10,13 @@ export default function Document() {
       <h1>Documentos</h1>
       <div className='document-list'>
         <ul id="list">
+          <li><a href='../../files/arquivos/A antropologia na esfera publica patrimônios culturais e museus.pdf' download="A antropologia na esfera publica patrimônios culturais e museus.pdf" type="application/pdf">A antropologia na esfera publica patrimônios culturais e museus</a></li>
+          <li><a href='../../files/arquivos/A colonização do sertão da Paraíba agentes produtores do espaço e contatos interétnicos.pdf' download={"A colonização do sertão da Paraíba agentes produtores do espaço e contatos interétnicos.pdf"} type="application/pdf">A colonização do sertão da Paraíba agentes produtores do espaço e contatos interétnicos</a></li>
           <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
           <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
           <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
           <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
-          <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
-          <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
-          <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
-          <li><a href='../../files/arquivos/Curriculo2022.pdf' download={"Curriculo2022.pdf"} type="application/pdf">Baixar Curriculo David Devolindo</a></li>
+          <li><a href='../../files/arquivos/Curriculo2022.pdf' download="Curriculo2022.pdf" type="application/pdf">Baixar Curriculo David Devolindo</a></li>
         </ul>
       </div>
       
