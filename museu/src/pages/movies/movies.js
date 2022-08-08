50f2b090a02c67232c85157d3f770c8793dcc4fa
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-08-08 14:13:07 -0300

    add video
    Co-Authored-By: Allan Gabriel <43339727+allangbr@users.noreply.github.com>

--- museu/src/pages/movies/movies.js
+++ museu/src/pages/movies/movies.js
@@ -24,8 +24,7 @@ export default function Movie() {
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/oG_KXm-QlLU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/vW5dFnxWv94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/QRwJzOYrLfg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
-            <li><iframe width="560" height="315" src="https://www.youtube.com/embed/wjrZ1WOMU8E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
-            <li></li>
+            <li><iframe width="560" height="315" src="https://www.youtube.com/embed/reVGQABDzuQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li></li>
             <li></li>
             <li></li>
