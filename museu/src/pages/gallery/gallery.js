2d1a59f703f072ddbd71ceb37c64727c21242ea1
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-08-17 16:20:39 -0300

    add imgs path 1 and 2 #14
    Co-Authored-By: Allan Gabriel <43339727+allangbr@users.noreply.github.com>

--- museu/src/pages/gallery/gallery.js
+++ museu/src/pages/gallery/gallery.js
@@ -3,17 +3,16 @@ import './gallery.css';
 import { AiFillCloseCircle } from "react-icons/ai";
 import Navbar from '../../components/Navbar/navbar';
 import Footer from "../../components/Footer/footer";
-import Img1 from '../../files/images/img1.jpg'
-import Img2 from '../../files/images/img2.jpg'
-import Img3 from '../../files/images/img3.jpg'
-import Img4 from '../../files/images/img4.jpg'
-import Img5 from '../../files/images/img5.jpg'
-import Img6 from '../../files/images/img6.jpg'
-import Img7 from '../../files/images/img7.jpg'
-import Img8 from '../../files/images/img8.jpg'
-import Img9 from '../../files/images/img9.jpg'
-import Img10 from '../../files/images/img10.jpg'
-import Img11 from '../../files/images/img11.jpg'
+import Img1 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img1.jpg'
+import Img2 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img2.jpg'
+import Img3 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img3.jpg'
+import Img4 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img4.jpg'
+import Img5 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img5.jpg'
+import Img6 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img6.jpg'
+import Img7 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img7.jpg'
+import Img8 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img8.jpg'
+import Img9 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img9.jpg'
+import Img10 from '../../files/images/Seminário de encerramento do curso Educação para as Relações Etnico-Raciais/img10.jpg'
 
 export default function Gallery() {
 
@@ -21,57 +20,52 @@ export default function Gallery() {
         {
         id:1,
         imgSrc: Img1,
-        description: 'O Rio Grande do Norte é conhecido pelo seu folclore, com as obras de Luis da Câmara Cascudo e de Mário de Andrade. Hoje, prefere-se designar como patrimônio imaterial as práticas sociais coletivas, como as festas e as celebrações, as formas de expressão cênicas, plásticas, musicais ou lúdicas e os saberes transmitidos de geração em geração (os ofício,s as técnicas e modos de fazer). Os povos tradicionais e seus mestres, poetas e contadores de história, artesões, rabequeiros, rezadeiras e curandeiros, cozinheiras são os detentores desse patrimônio pouco valorizado. As festas, como as da irmandade do Rosário no Seridó, as brincadeiras e as danças, têm um atrativo especial por nos mostrar que esses saberes da tradição são muito vivos.'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais.'
         },
         {
         id:2,
         imgSrc: Img2,
-        description: 'teste2'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:3,
         imgSrc: Img3,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:4,
         imgSrc: Img4,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:5,
         imgSrc: Img5,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:6,
         imgSrc: Img6,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:7,
         imgSrc: Img7,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:8,
         imgSrc: Img8,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:9,
         imgSrc: Img9,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
         {
         id:10,
         imgSrc: Img10,
-        description: 'beiçano'
-        },
-        {
-        id:11,
-        imgSrc: Img11,
-        description: 'beiçano'
+        description: 'Seminário de encerramento do curso Educação para as Relações Etnico-Raciais'
         },
     ]
 
--- museu/src/pages/movies/movies.js
+++ museu/src/pages/movies/movies.js
@@ -38,6 +38,7 @@ export default function Movie() {
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/3JPh55gFQps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/wylOk1V4WgI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
             <li><iframe width="560" height="315" src="https://www.youtube.com/embed/Xnvf4_jOXJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+
           </ul>
         </div>
       </div>
